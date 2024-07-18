3305e2fbaf68fc679678a8c9eec59d9fac7341fb
rahmat131 <rahmatdevs131@gmail.com>
2024-07-18 15:40:16 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
+      <header>
+      <nav>
+        <ul>
+          <li><a href="/home">Homepage</a></li>
+          <li><a href="/about">About Us</a></li>
+          <li li><a href="/blog">Blog</a></li>  
+        </ul>
+      </nav>
+      </header>
+      <main>
+
       <h1>Hello! World!</h1>
+      </main>
+      <footer>
+        Copyright
+      </footer>
+
     </div>
   );
 }
