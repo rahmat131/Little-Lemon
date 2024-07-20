11f725eaad19c0cac6c9bd5f1e23ea6cebe7c3a9
rahmat131 <rahmatdevs131@gmail.com>
2024-07-20 19:27:45 +0200

    Main Updated!

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,9 +1,22 @@
+import restpic from './Images/restaurant.jpg'
 function Main() {
     return (
       
       
-        <main class="mainclass">
-        <h1>Main Content!</h1>
+        <main>
+            <div class="Hero">
+                <div class="Left">
+                    <h1>Little Lemon</h1>
+                    <h3>Chicago</h3>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+                    </p>
+                    <button>Reserve a Table</button>
+                </div>
+                <div class="Right">
+                <img src={restpic} alt="Description of my image" />
+                </div>
+            </div>
         </main>
       
      
